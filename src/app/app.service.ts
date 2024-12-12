a785c9ccf0820c1880f557d5ace1326a43079422
Inaê Rosa <inaedb.rosa@gmail.com>
2024-12-11 21:16:05 -0300

    feat: make a sum

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const a = 1;
+    const b = 2;
+    const result = a + b;
+    console.log(result);
     return 'Hello World!';
   }
 }
